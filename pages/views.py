caa6bba425e10ce239dfd7f2f5107134e954d909
shrikanta <shrikantadutta108@gmail.com>
2025-11-24 19:18:15 +0600

    completed search functionalities and admin customization

--- pages/views.py
+++ pages/views.py
@@ -7,10 +7,18 @@ def home(request):
     teams = Team.objects.all()
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_date')
+    model_search = Car.objects.values('model',).distinct()
+    city_search = Car.objects.values('city',).distinct()
+    year_search = Car.objects.values('year',).distinct()
+    body_style_search = Car.objects.values('body_style',).distinct()
     context = {
         'teams':teams,
         'featured_cars':featured_cars,
         'all_cars':all_cars,
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style_search':body_style_search,
     }
     return render(request,'pages/home.html',context)
 
